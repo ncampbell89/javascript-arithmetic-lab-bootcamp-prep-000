9e8245755487e2d1534ee8b34be8db9124c9107e
Nicole Campbell <nscampbell303@gmail.com>
2017-09-28 17:10:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,11 +25,9 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  parseInt('n', 10)
-  return parseInt()
+  return parseInt('n', 10)
 }
 
 function preserveDecimal(n) {
-  parseFloat('n', 10)
-  return parseFloat()
+  return parseFloat('n', 10)
 }
